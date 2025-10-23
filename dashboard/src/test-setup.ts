a2fba7e1a03ad04f607cf167be9cdd3ec982bf32
hachall <hachall@hotmail.com>
2025-10-23 19:02:58 +0100

    frontend tests passing

--- dashboard/src/test-setup.ts
+++ dashboard/src/test-setup.ts
@@ -1,5 +1,15 @@
 import "@testing-library/jest-dom";
 
+// Polyfill ResizeObserver for Radix UI and other libs in Vitest/JSDOM
+if (typeof window !== "undefined" && !window.ResizeObserver) {
+  window.ResizeObserver = class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+}
+
+
 // Mock environment variables
 Object.defineProperty(import.meta, "env", {
   value: {
