88ef36493cf0649e3966405b28852778e8f143a3
Fergus <fergus.barratt00@gmail.com>
2025-11-08 15:36:16 +0000

    fix: enable endpoints in tests

--- dwctl/src/test_utils.rs
+++ dwctl/src/test_utils.rs
@@ -116,6 +116,7 @@ pub fn create_test_config() -> crate::config::Config {
         enable_request_logging: false,
         enable_otel_export: false,
         batches: BatchConfig {
+            enabled: true,
             files: FilesConfig {
                 max_file_size: 1000 * 1024 * 1024, //1GB
                 ..Default::default()
