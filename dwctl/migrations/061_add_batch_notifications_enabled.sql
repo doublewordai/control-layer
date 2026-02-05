0403ad61ea497ef29cdfc14805c1e3444cf898b0
josh <joshuacowan14@gmail.com>
2026-02-05 17:33:21 +0000

    we want notification system on by default

--- dwctl/migrations/061_add_batch_notifications_enabled.sql
+++ dwctl/migrations/061_add_batch_notifications_enabled.sql
@@ -1,2 +1,2 @@
--- Add per-user toggle for batch completion email notifications
-ALTER TABLE users ADD COLUMN batch_notifications_enabled BOOLEAN NOT NULL DEFAULT false;
+-- Add per-user toggle for batch completion email notifications (default on)
+ALTER TABLE users ADD COLUMN batch_notifications_enabled BOOLEAN NOT NULL DEFAULT true;
