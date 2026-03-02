b11fd877550608fbbf3c1ac9007fde3a187b1af0
josh <joshuacowan14@gmail.com>
2026-03-02 18:21:54 +0000

    idempotency

--- dwctl/migrations/071_add_low_balance_notified.sql
+++ dwctl/migrations/071_add_low_balance_notified.sql
@@ -1,2 +1,2 @@
-ALTER TABLE users ADD COLUMN low_balance_notification_sent BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE users ADD COLUMN low_balance_threshold REAL DEFAULT NULL;
+ALTER TABLE users ADD COLUMN IF NOT EXISTS low_balance_notification_sent BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE users ADD COLUMN IF NOT EXISTS low_balance_threshold REAL DEFAULT NULL;
